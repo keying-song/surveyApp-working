148f92f640a01808a58435c361a9dc70267a57b3
sukhmandeep100 <109261511+sukhmandeep100@users.noreply.github.com>
2022-07-14 18:32:44 -0400

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
-const Connection = async(url)=>{
-    const URL = url;
+const Connection = async()=>{
+    const URL = "mongodb://localhost:27017";
     try{
-       await mongoose.connect(URL, {
+       await mongoose.connect("mongodb://localhost:27017", {
             useNewUrlParser:true,
             useUnifiedTopology: true,})
             console.log(`database connected successfully`)
