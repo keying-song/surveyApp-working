40e8cf57161134d59d115f64461a7373dd432920
sukhmandeep100 <109261511+sukhmandeep100@users.noreply.github.com>
2022-07-14 18:35:50 -0400

    Update user-schema.js

--- server/schema/user-schema.js
+++ server/schema/user-schema.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 import autoIncrement from "mongoose-auto-increment";
 
 const userSchema = mongoose.Schema({
-    name:String,
-    phone:String,
-    email:String
+    Username :String,
+    Password :String,
+    
 })
 autoIncrement.initialize(mongoose.connection);
 userSchema.plugin(autoIncrement.plugin, 'user');
